17e6a835a20663fecc4da6e7067c96e53217d7b4
Marko Melko <marko.melko@g133957.elisakoti>
2023-01-25 11:36:41 +0200

    Vercel build test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CONTENTFUL_SPACE: "FROM_CONTENTFUL",
-    CONTENTFUL_SPACE_KEY: "FROM_CONTENTFUL",
-  },
 };
 
 module.exports = nextConfig;
